e3a929b4e68f114823de0448071e8ac96bfb9e7c
Emerald Chen <ahyuancy@gmail.com>
2019-01-11 14:31:29 +1300

    Update .js

--- .js
+++ .js
@@ -2,10 +2,7 @@ function steamrollArray(arr) {
   let flat = [].concat(...arr);
   return flat.some(Array.isArray) ? steamrollArray(flat) : flat;
 }
-
-
-//The Array.isArray() method determines whether the passed value is an Array.
-
+// The Array.isArray() method determines whether the passed value is an Array.
 // The some() method tests whether at least one element in the array passes the test implemented by the provided function.
 
 steamrollArray([[["a"]], [["b"]]]); // return ["a", "b"]
