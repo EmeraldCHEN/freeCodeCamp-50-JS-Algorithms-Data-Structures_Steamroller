9f978b3fa55fd788ae68ab4328b43860ec65fa46
Emerald Chen <ahyuancy@gmail.com>
2019-01-11 15:27:34 +1300

    Update .js

--- .js
+++ .js
@@ -11,6 +11,28 @@ steamrollArray([1, {}, [3, [[4]]]]); // return [1, {}, 3, 4]
 
 
 
+/************  Solution #2     *******************************************************************************************/
+
+function steamrollArray(arr) {
+ // Firstly turn the array to a string of numbers seperated by a comma, 
+ //  double comma if there was an empty array and literal object text if there was an object
+  return arr.toString().replace(',,', ',')  // "1,2,,3" => "1,2,3"
+         .split(',').map(function(x){
+               if(x == '[object Object]'){  // bring back empty objects
+                    return {};
+               }else if(isNaN(x)){  // if not a number (string)
+                    return x;
+               }else{   // if a number in a string, convert it
+                    return parseInt(x);
+               }
+  });
+
+}
+
+
+
+
+
 
 
 // Retrieved from https://guide.freecodecamp.org/certifications/javascript-algorithms-and-data-structures/intermediate-algorithm-scripting/steamroller/
